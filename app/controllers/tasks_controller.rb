f115735a1c87308eecf946b5d4bf46a2c54691fc
ryosuke <123ryo123456@gmail.com>
2023-03-26 09:17:19 +0000

    make task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,12 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
+  
   def index
-    @tasks=Task.all
-    @pagy,@tasks = pagy(Task.all, items: 3)
+    @tasks = current_user.tasks.build
+    @pagy,@tasks = pagy(current_user.tasks.order(id: :desc))
   end
 
   def show
@@ -14,12 +17,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success]='Taskが正常に登録されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @pagy, @tasks=pagy(current_user.tasks.order(id: :desc))
       flash.now[:danger]='Taskが登録されませんでした'
       render :new
     end
@@ -48,10 +52,17 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task=Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
