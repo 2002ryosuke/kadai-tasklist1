3ebca769a206b7f06c743ee31500a5a5060eba36
ryosuke <123ryo123456@gmail.com>
2023-03-29 08:22:20 +0000

    修正２

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,6 @@ class TasksController < ApplicationController
   
   
   def index
-    @tasks = current_user.tasks.build
     @pagy,@tasks = pagy(current_user.tasks.order(id: :desc))
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+before_action :require_user_logged_in, only: [:show]
+
 
   def show
     @user =User.find(params[:id])
     @pagy,@tasks = pagy(@user.tasks.order(id: :desc))
-    counts(@user)
   end
 
   def new
